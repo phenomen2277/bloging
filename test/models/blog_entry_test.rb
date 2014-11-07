b96f684ba51b507832e055eef203cc665dc62004
Jone Samra <jonemob@gmail.com>
2014-11-07 20:18:40 +0100

    (RED) - should not save the blog entry when the tag_list is empty

--- test/models/blog_entry_test.rb
+++ test/models/blog_entry_test.rb
@@ -18,4 +18,12 @@ class BlogEntryTest < ActiveSupport::TestCase
 		assert_not blog_entry.save
 	end
 
+	test "should not save the blog entry when the tag_list is empty" do 
+		blog_entry = BlogEntry.new
+		blog_entry.title = "The is the title of the blog entry"
+		blog_entry.body = "The is the body of the blog entry"
+		assert_not blog_entry.save		
+
+	end
+
 end
