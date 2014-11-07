744e62b7bfc969bacd51b0974f7abacdd791a727
Jone Samra <jonemob@gmail.com>
2014-11-07 21:26:30 +0100

    (REFACTOR) The model BlogEntry takes care of the tag_list
    The model will add ”Unspecified” tag if there is no tag in the tag_list

--- app/models/blog_entry.rb
+++ app/models/blog_entry.rb
@@ -3,4 +3,12 @@ class BlogEntry < ActiveRecord::Base
 	validates :title, :presence => true
 	validates :body, :presence => true
 	validates :tag_list, :presence => true
+	before_validation :check_tags
+
+	private
+	def check_tags 
+		if self.tag_list.empty?
+		self.tag_list.add("Unspecified")
+		end
+	end
 end
--- test/models/blog_entry_test.rb
+++ test/models/blog_entry_test.rb
@@ -2,35 +2,38 @@ require "test_helper"
 
 class BlogEntryTest < ActiveSupport::TestCase 
 
-	test "should initialize a new blog entry object" do
-		blog_entry = BlogEntry.new
-	end
+test "should initialize a new blog entry object" do
+	blog_entry = BlogEntry.new
+end
 
-	test "should not save the blog entry when title is not given" do
-		blog_entry = BlogEntry.new
-		blog_entry.body = "The is the body of the blog entry"
-		assert_not blog_entry.save
-	end
+test "should not save the blog entry when title is not given" do
+	blog_entry = BlogEntry.new
+	blog_entry.body = "The is the body of the blog entry"
+	assert_not blog_entry.save
+end
 
-	test "should not save the blog entry when body is not given" do
-		blog_entry = BlogEntry.new
-		blog_entry.title = "The is the title of the blog entry"
-		assert_not blog_entry.save
-	end
+test "should not save the blog entry when body is not given" do
+	blog_entry = BlogEntry.new
+	blog_entry.title = "The is the title of the blog entry"
+	assert_not blog_entry.save
+end
 
+#Refactoring so that the model BlogEntry add an "Unspecified" tag if no tag has been added
+=begin
 	test "should not save the blog entry when the tag_list is empty" do 
 		blog_entry = BlogEntry.new
 		blog_entry.title = "The is the title of the blog entry"
 		blog_entry.body = "The is the body of the blog entry"
 		assert_not blog_entry.save		
 	end
+=end
 
-	test "should save a new blog entry" do
-		blog_entry = BlogEntry.new
-		blog_entry.title = "The is the title of the blog entry"
-		blog_entry.body = "The is the body of the blog entry" 
-		blog_entry.tag_list.add("Unspecified")
-		assert blog_entry.save
-	end
+test "should save a new blog entry" do
+	blog_entry = BlogEntry.new
+	blog_entry.title = "The is the title of the blog entry"
+	blog_entry.body = "The is the body of the blog entry" 
+	blog_entry.tag_list.add("Unspecified")
+	assert blog_entry.save
+end
 
 end
