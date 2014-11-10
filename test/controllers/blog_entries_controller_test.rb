ccbc836ce867f913b4f00be6ed5934f96fe6795a
Jone Samra <jonemob@gmail.com>
2014-11-10 16:25:02 +0100

    (RED) - should not create new blog entry

--- test/controllers/blog_entries_controller_test.rb
+++ test/controllers/blog_entries_controller_test.rb
@@ -20,5 +20,12 @@ class BlogEntriesControllerTest < ActionController::TestCase
 	assert_redirected_to blog_entries_path
 	end
 
+	test "should not create new blog entry" do 
+	be = blog_entries(:one)
+	BlogEntry.expects(:new).at_least(1).returns(be)
+	@blog_entry = BlogEntry.new
+	post :create, blog_entry: { body: @blog_entry.body, title: @blog_entry.title, tag_list: :tag_one }
+	assert_response :forbidden
+	end
 
 end
