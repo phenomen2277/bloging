87019d2fdcf32be653a14fb4de97a80b3d19a307
Jone Samra <jonemob@gmail.com>
2014-11-11 11:39:41 +0100

    (GREEN) - should submit a new blog entry

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -6,11 +6,13 @@ class UserFlowTest < ActionDispatch::IntegrationTest
 		Capybara.default_wait_time = 15
 		Capybara.current_driver = :selenium
 	end
-	
-	test "should submit a new blog entry" do
+
+	test "should submit a new blog entry" do 
 		visit(new_blog_entry_path)
 		fill_in("Title", :with => "This is the title")
-		click_on("Create")
-		assert page.has_content?('created.')
+		fill_in("Body", :with => "This is the body")
+		fill_in("Tags", :with => "Foo, bar")
+		click_on("Create Blog entry")
+		assert page.has_content?('This is the title')
 	end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'simplecov'
 require 'capybara'
+require 'capybara/rails'
 require 'capybara/dsl'
 
 SimpleCov.start
