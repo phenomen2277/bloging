801fe2ac4414195e2cba4033ce601b8f80882e9a
Jone Samra <jonemob@gmail.com>
2014-11-11 11:32:33 +0100

    (RED) - should submit a new blog entry

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -6,5 +6,11 @@ class UserFlowTest < ActionDispatch::IntegrationTest
 		Capybara.default_wait_time = 15
 		Capybara.current_driver = :selenium
 	end
-
+	
+	test "should submit a new blog entry" do
+		visit(new_blog_entry_path)
+		fill_in("Title", :with => "This is the title")
+		click_on("Create")
+		assert page.has_content?('created.')
+	end
 end
